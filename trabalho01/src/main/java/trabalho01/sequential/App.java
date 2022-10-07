672a3cc0073a7fe40024c4480039a287b17f8635
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-10-07 20:01:19 -0300

    fix: nome da implementação sequencial

--- trabalho01/src/main/java/trabalho01/sequential/App.java
+++ trabalho01/src/main/java/trabalho01/sequential/App.java
@@ -1,4 +1,4 @@
-package trabalho01.iterative;
+package trabalho01.sequential;
 
 import java.io.IOException;
 
@@ -6,7 +6,9 @@ import trabalho01.utils.File;
 import trabalho01.utils.Task;
 
 public class App {
+
     /**
+     * Main method to start the sequential experiment. 
      * @param args
      * @throws IOException
      * @throws InterruptedException
@@ -14,19 +16,19 @@ public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
         int numOfExecutions = 21;
         int[] scenarios = { 5, 10, 30, 50, 100, 150, 300 };
-        String type = "iterative";
+        String type = "sequential";
 
         Task task = new Task();
 
         for (int scenario = 0; scenario < scenarios.length; scenario++) {
-            long start = System.currentTimeMillis();
             for (int execution = 0; execution < numOfExecutions; execution++) {
+                long start = System.currentTimeMillis();
                 for (int index = 0; index < scenarios[scenario]; index++) {
                     task.run();
                 }
+                long elapsed = System.currentTimeMillis() - start;
+                File.writeResult(type, scenarios[scenario], elapsed);
             }
-            long elapsed = System.currentTimeMillis() - start;
-            File.writeResult(type, scenarios[scenario], elapsed);
         }
     }
 }
