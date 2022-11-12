b9d988d26829c8faffa2b95f6c9874769302b898
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-11-12 11:26:23 -0300

    feat: operação do número de Euler

---
+++ trabalho02/src/main/java/javaconcurrencyutilities/NumbereCalculator/Factorial.java
@@ -0,0 +1,42 @@
+package javaconcurrencyutilities.NumbereCalculator;
+
+import java.math.BigDecimal;
+import java.util.concurrent.Callable;
+
+
+public class Factorial implements Callable<BigDecimal> {
+	private BigDecimal number;
+
+	/**
+	 * Parameterized constructor
+	 * @param number Number to calculate factorial
+	 */
+	public Factorial(BigDecimal number) {
+		this.number = number;
+	}
+
+	/**
+	 * Task to run upon a thread
+	 * @return Factorial of a given number
+	 */
+	@Override
+	public BigDecimal call() {
+		return factorial(this.number);
+	}
+
+	/**
+	 * Recursively calculates factorial of a given number
+	 * @param number Number to calculate factorial
+	 * @return Factorial of the number
+	 */
+	private BigDecimal factorial(BigDecimal number) {
+		BigDecimal value = new BigDecimal(1);
+		if (value.compareTo(number) == 1 || value.compareTo(number) == 0) {
+			return value;
+		} else {
+			return number.multiply(factorial(number.subtract(value)));
+		}
+	}
+}
+
+
--- trabalho02/src/main/java/javaconcurrencyutilities/NumbereCalculator/NumbereCalculator.java
+++ trabalho02/src/main/java/javaconcurrencyutilities/NumbereCalculator/NumbereCalculator.java
@@ -1,42 +1,11 @@
 package javaconcurrencyutilities.NumbereCalculator;
 
 import java.math.BigDecimal;
-import java.util.concurrent.Callable;
+import java.math.MathContext;
 
-
-public class NumbereCalculator implements Callable<BigDecimal> {
-	private BigDecimal number;
-
-	/**
-	 * Parameterized constructor
-	 * @param number Number to calculate factorial
-	 */
-	public NumbereCalculator(BigDecimal number) {
-		this.number = number;
-	}
-
-	/**
-	 * Task to run upon a thread
-	 * @return Factorial of a given number
-	 */
-	@Override
-	public BigDecimal call() {
-		return factorial(this.number);
-	}
-
-	/**
-	 * Recursively calculates factorial of a given number
-	 * @param number Number to calculate factorial
-	 * @return Factorial of the number
-	 */
-	private BigDecimal factorial(BigDecimal number) {
-		BigDecimal value = new BigDecimal(1);
-		if (value.compareTo(number) == 1 || value.compareTo(number) == 0) {
-			return value;
-		} else {
-			return number.multiply(factorial(number.subtract(value)));
-		}
-	}
+public class NumbereCalculator {
+    public BigDecimal sumFactorial(BigDecimal factorial, BigDecimal sum) {
+        BigDecimal value = new BigDecimal(1);
+        return sum.add(value.divide(factorial, MathContext.DECIMAL128));
+    }
 }
-
-
