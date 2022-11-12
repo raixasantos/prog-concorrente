5789adf365371d4af5b1075749acc289742fefbd
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-11-12 18:10:09 -0300

    feat: documentação
    Co-authored-by: Neylane Lopes <neylanepl@users.noreply.github.com>

--- trabalho02/src/handleinput/HandleInput.java
+++ trabalho02/src/handleinput/HandleInput.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HandleInput {
+    
+    /** 
+     * Handle command line to get number of numerical terms and number of threads to execute.
+     * @param args The arguments of the program.
+     * @return int[] Number of numerical terms and number of threads to execute.
+     */
     public static int[] handleArgsInput(String[] args) {
         int[] params = new int[] { 0, 0 };
         if (args.length == 0) {
@@ -11,7 +17,7 @@ public class HandleInput {
         }
 
         for (int i = 0; i < args.length; i++) {
-            if (i == 1) break;
+            if (i == 2) break;
             try {
                 params[i] = Integer.parseInt(args[i]);
             }
--- trabalho02/src/numberecalculator/NumbereCalculator.java
+++ trabalho02/src/numberecalculator/NumbereCalculator.java
@@ -4,6 +4,13 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 public class NumbereCalculator {
+    
+    /** 
+     * Sum of divisions from 1 factorial with 0 to n.
+     * @param factorial Number of factorial.
+     * @param sum Previous sum value.
+     * @return BigDecimal Result of new sum.
+     */
     public BigDecimal sumFactorial(BigDecimal factorial, BigDecimal sum) {
         BigDecimal value = new BigDecimal(1);
         return sum.add(value.divide(factorial, MathContext.DECIMAL128));
