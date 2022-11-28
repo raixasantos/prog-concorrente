7a5ebf910b0b854e06f20a03d0384d81f89a9c3f
Neylane <neylanepl@gmail.com>
2022-11-28 19:16:26 -0300

    feat: condições para pessoas  entrar no banheiro

--- trabalho03/src/main/java/banheirounissex/Bathroom.java
+++ trabalho03/src/main/java/banheirounissex/Bathroom.java
@@ -9,6 +9,9 @@ public class Bathroom {
 
 	private Queue<Person> bathroom;
 
+	private char currentGender = 'N';
+	private Queue<Person> queue;
+
 
 	public Bathroom(int capacity) {
 		this.capacity = capacity;
@@ -26,7 +29,42 @@ public class Bathroom {
 		}
 		System.out.println("Vai entrar no banheiro o gênero: " + person.getGender());
 		// TODO: adicionar condição de qual gênero entra
-		bathroom.add(person);
+		//se o banheiro está cheio de mulheres e uma mulher quer entrar, vai pra fila?
+		if(bathroom.size() == 0 && currentGender == 'N') {
+			bathroom.add(person);
+			currentGender = person.getGender();
+		}
+		else if(bathroom.size() == 0 && (currentGender == 'F' || currentGender == 'N') && queue.peek().getGender() == 'F' ){
+			bathroom.add(person);
+			currentGender = queue.peek().getGender();
+			queue.poll();
+		}
+		else if(bathroom.size() == 0 && (currentGender == 'M' || currentGender == 'N') && queue.peek().getGender() == 'M' ){
+			bathroom.add(person);
+			currentGender = queue.peek().getGender();
+			queue.poll();
+		}
+		else if(bathroom.size() < 5 && (currentGender == 'F' || currentGender == 'N') && queue.peek().getGender() == 'F' ){
+			bathroom.add(person);
+			currentGender = queue.peek().getGender();
+			queue.poll();
+		}
+		else if(bathroom.size() < 5 && (currentGender == 'M' || currentGender == 'N') && queue.peek().getGender() == 'M' ){
+			bathroom.add(person);
+			currentGender = queue.peek().getGender();
+			queue.poll();
+		}
+		else if(bathroom.size() < 5 && currentGender == 'F' && person.getGender() == 'F'){
+			bathroom.add(person);
+			currentGender = person.getGender();
+		}
+		else if(bathroom.size() < 5 && currentGender == 'M' && person.getGender() == 'M'){
+			bathroom.add(person);
+			currentGender = person.getGender();
+		}
+		else {
+			queue.add(person);
+		}
 		System.out.println("Entrou no banheiro: " + person.getId() + ". Qtd no banheiro: " + bathroom.size());
 		notify();
 	}
