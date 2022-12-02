1da7f49beb1616c2ab3254ef1d48afb9f0f3691c
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-12-01 23:21:17 -0300

    feat: forma de espera das threads
    Co-authored-by: Neylane Lopes <neylanepl@users.noreply.github.com>

--- trabalho03/src/main/java/banheirounissex/Bathroom.java
+++ trabalho03/src/main/java/banheirounissex/Bathroom.java
@@ -1,50 +1,29 @@
 package banheirounissex;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bathroom {
     	
 	private int capacity;
-	private LinkedList<Person> peopleInBathroom;
+	private List<Person> peopleInBathroom;
 
 	private char currentGender = 'N';
-	private LinkedList<Person> waitingList;
 
 
 	public Bathroom(int capacity) {
 		this.capacity = capacity;
-		waitingList = new LinkedList<>();
-		peopleInBathroom = new LinkedList<>();
+		peopleInBathroom = new ArrayList<>();
 	}
 
 
-	public synchronized void insert(Person person) {
-		System.out.println("Vai entrar na fila do banheiro. gênero: " + person.getGender() + " id: " + person.getId());
-		waitingList.add(person);
-
-		if(peopleInBathroom.isEmpty()) {
-			currentGender = waitingList.peek().getGender();
-			Person headPerson = waitingList.poll();
-			peopleInBathroom.add(headPerson);
-
-			System.out.println("Banheiro vazio. gênero: " + person.getGender() + " id: " + person.getId());
-			System.out.println("Começou a usar o banheiro. gênero: " + person.getGender() + " id: " + person.getId()
-				+ ". Qtd no banheiro: " + peopleInBathroom.size());
-		}
-		else {
-			if(peopleInBathroom.size() < capacity && currentGender == person.getGender()) {
-				System.out.println("Genero atual: " + currentGender + " Genero de quem entrou: " + waitingList.peek().getGender());
-				Person headPerson = waitingList.poll(); // trocar para remover a pessoa em especifíco
-				peopleInBathroom.add(headPerson);
-				System.out.println("Começou a usar o banheiro. gênero: " + person.getGender() + " id: " + person.getId()
-					+ ". Qtd no banheiro: " + peopleInBathroom.size());	
-			}
-		}
+	public synchronized void enter(Person person) {
+		System.out.println("Quer entrar no banheiro. gênero: " + person.getGender() + " id: " + person.getId());
 
 		while (peopleInBathroom.size() == capacity) {
-			System.out.println("Banheiro cheio! Ids: ");
+			System.out.println("Banheiro cheio! Ids:");
 			for (Person per : peopleInBathroom) {
-				System.out.print(per.getId() + " ");
+				System.out.print(" " + per.getId() + "  gênero: " + person.getGender());
 			}
 			try {
 				wait();
@@ -54,29 +33,38 @@ public class Bathroom {
 				Thread.currentThread().interrupt();
 			}
 		}
-			
+
+		while (currentGender != person.getGender() && currentGender != 'N') {
+			System.out.println("Não posso entrar, gen: " + person.getGender() + " genero atual: " + currentGender);
+			try {
+				wait();
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				Thread.currentThread().interrupt();
+			}
+		}
+
+		peopleInBathroom.add(person);
+		if(currentGender == 'N') {
+			currentGender = person.getGender();
+		}
+		System.out.println("Começou a usar o banheiro. gênero: " + person.getGender() + " id: " + person.getId()
+				+ ". Qtd no banheiro: " + peopleInBathroom.size());
 		notify();
 	}
 
-	public synchronized void remove(Person person) {
+	public synchronized void leave(Person person) {
 		System.out.println("Tentando sair do banheiro. id " + person.getId());
-		/* 
-		while (peopleInBathroom.isEmpty()) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				Thread.currentThread().interrupt();
-			}
-		}		
-		*/
 		Boolean personRemoved = peopleInBathroom.remove(person);
         if(Boolean.TRUE.equals(personRemoved)){
             System.out.println("Saiu do banheiro " + person.getId() 
 				+ " do gênero " + person.getGender() 
 				+ ". Qtd no banheiro: " + peopleInBathroom.size());
         }
+		if(peopleInBathroom.isEmpty()) {
+			currentGender = 'N';
+		}
 		notify();
-		// return;
 	}
 }
