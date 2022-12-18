755fe661dd249ea62c771a034bb4a853f9fe9398
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-12-18 15:17:31 -0300

    feat: struct Player

--- trabalho04/tenis.go
+++ trabalho04/tenis.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Player represents a player in a game.
+type Player struct {
+	id, points int
+}
+
+// NewPlayer returns an empty player of the specified id and points.
+func NewPlayer(id, points int) *Player {
+	return &Player{id: id, points: points}
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	player01 := NewPlayer(1, 0)
+	player02 := NewPlayer(2, 0)
+	fmt.Println("Hello, World! Número sorteado:", rand.Intn(1))
+	fmt.Println("Id do jogador 01:", player01.id)
+	fmt.Println("Id do jogador 02:", player02.id)
+	time.Sleep(time.Second / 30)
 }
