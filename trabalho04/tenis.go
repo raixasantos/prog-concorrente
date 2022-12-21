89db4e4b9857a0cc0e16dd0b30e6f37333cfc397
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-12-20 23:10:40 -0300

    feat: tratamento do problema com channel
    Co-authored-by: Neylane Lopes <neylanepl@users.noreply.github.com>

--- trabalho04/tenis.go
+++ trabalho04/tenis.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func Random() int {
+	randomNumber := rand.Intn(2)
+	return randomNumber
+}
+
+func makePoint() int {
+	return Random()
+}
 
 // Player represents a player in a game.
 type Player struct {
@@ -16,20 +30,51 @@ func NewPlayer(id, points int) *Player {
 	return &Player{id: id, points: points}
 }
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
+var bestPlayer Player
+
+func updateWinner(player Player) {
+	if player.points > bestPlayer.points {
+		bestPlayer.id = player.id
+		bestPlayer.points = player.points
+	}
 }
 
-func Random() int {
-	randomNumber := rand.Intn(2)
-	return randomNumber
+func move(player Player, court chan bool, waitGroup *sync.WaitGroup) {
+	for bestPlayer.points < 7 {
+		status := <-court
+		if !status {
+			return
+		}
+		fmt.Println(status)
+		fmt.Println("Starting move")
+		if makePoint() == 1 {
+			player.points++
+			fmt.Println("Player", player.id, "Points: ", player.points)
+		}
+		updateWinner(player)
+		court <- true
+	}
+
 }
 
 func main() {
+	var waitGroup sync.WaitGroup
+	court := make(chan bool)
+
 	player01 := NewPlayer(1, 0)
 	player02 := NewPlayer(2, 0)
-	fmt.Println("Hello, World! Número sorteado:", Random())
-	fmt.Println("Id do jogador 01:", player01.id)
-	fmt.Println("Id do jogador 02:", player02.id)
-	time.Sleep(time.Second / 30)
+
+	waitGroup.Add(2)
+	fmt.Println("Starting")
+	go move(*player01, court, &waitGroup)
+	go move(*player02, court, &waitGroup)
+	court <- true
+
+	fmt.Println("Finishing channel", <-court)
+	for i := 0; i < 2; i++ {
+		waitGroup.Done()
+	}
+
+	waitGroup.Wait()
+	fmt.Println("Best player:", bestPlayer.id, " Points:", bestPlayer.points)
 }
